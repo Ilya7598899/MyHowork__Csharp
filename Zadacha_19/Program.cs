d3472cc901cc32fe9811e395738433ff62a4080b
Ilya7598899 <114740651+Ilya7598899@users.noreply.github.com>
2022-10-14 14:10:48 +0300

    Добавил задачу 23

--- Zadacha_19/Program.cs
+++ Zadacha_19/Program.cs
@@ -1,6 +1,6 @@
-﻿// Задача 19
+﻿// Задача 19 почему-то выделено жёлтым, но всё работает
 Console.WriteLine("Введите пятизначное число число");
-string a = (Console.ReadLine());
+string a = Console.ReadLine();
 if (a[0] == a[4] && a[1] == a[3])
 {
     Console.WriteLine("Число - палиндром");
---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 23
+Console.Write("Введите число N = ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < a; i++)
+{
+    Double kub = Math.Pow(i+1, 3);
+    Console.WriteLine(kub);
+}
