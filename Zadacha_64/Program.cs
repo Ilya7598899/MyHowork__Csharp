a0bf0ca75cde75f77183b0b538b784f8029de371
Ilya7598899 <114740651+Ilya7598899@users.noreply.github.com>
2022-11-17 17:39:00 +0300

    Сделал задачи 64 и 66

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64
+int PrintValue(int n)
+{
+    if (n==1) return 1;
+    else return (n-1);
+
+}
+
+int n=5;
+for (int i = n+1; i > 1; i--)
+{
+    Console.Write($"{PrintValue(i)}, ");
+}
+Console.WriteLine();
+n=8;
+for (int i = n+1; i > 1; i--)
+{
+    Console.Write($"{PrintValue(i)}, ");
+}
---
+++ Zadacha_66/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 66
+int PrintValue(int N)
+{
+    if (N == 1) return 1;
+    else return N + PrintValue(N-1);
+    }
+    Console.WriteLine(PrintValue(15));
+    Console.Write((PrintValue(8)-6));
